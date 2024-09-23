5cb4de5ddd648c58c043604f8d657ce363283ad4
Hilian <hilian.veloso@alumnos.liceolaportada.cl>
2024-09-23 12:09:53 -0300

    v3

--- Desafio_GitHub/Masa.py
+++ Desafio_GitHub/Masa.py
@@ -1,23 +1,23 @@
 def Kilo_Gram(num):
     g = num * 1000
-    print(g)
+    print(f"Los Kilos ingresados equivalen a {g} Gramos.")
 
 def Kilo_Ton(num):
     t = num / 1000
-    print(t)
+    print(f"Los Kilos ingresados equivalen a {t} Toneladas.")
 
 def Gram_Kilo(num):
     kg = num / 1000
-    print(kg)
+    print(f"Los Gramos ingresados equivalen a {kg} Kilos.")
 
 def Gram_Ton(num):
     t = num * 1000000
-    print(t)
+    print(f"Los Gramos ingresados equivalen a {t} Toneladas.")
 
 def Ton_Kilo(num):
     kg =num * 1000
-    print(kg)
+    print(f"Las Toneladas ingresadas equivalen a {kg} Kilos.")
 
 def Ton_Gram(num):
     g = num / 1000000
-    print(g)
+    print(f"Las Toneladas ingresadas equivalen a {g} Gramos.")
